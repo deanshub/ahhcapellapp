69666624794f3b55542ee3382372a85e7f592742
Kfir Stri <kfirstar@gmail.com>
2014-06-20 17:49:18 +0300

    Final changes before Master

--- Haloaa/www/js/index.js
+++ Haloaa/www/js/index.js
@@ -31,6 +31,7 @@ var app = {
     // Bind the buttons
     document.getElementById('people').addEventListener('click', this.onRecButtonClick, false);
     document.getElementById('logo').addEventListener('click', this.onLogoClick, false);
+    document.getElementById('songsButton').addEventListener('click', this.onSongsButtonClick, false);
     
   },
   // deviceready Event Handler
@@ -54,5 +55,7 @@ var app = {
   onLogoClick: function() {
     // Do something for logo
     navigator.notification.alert('Pressed logo', function () {}, 'Logo', 'Yeah!');
+  },
+  onSongsButtonClick: function() {
   }
 };
